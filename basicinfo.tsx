f13499b383b8e52576c35aa243431ff6ab6c168e
BrishnaN <brishna@udel.edu>
2024-09-07 15:58:03 -0400

    Branch changed

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -3,9 +3,9 @@ below variables and update them with stuff about you. Yeah... I know... the clas
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
 const yourName: String = "Brishna";
-let yourPronouns: String = "SHE/Her";
-const yourMajor: String = "Computer Scinece";
+let yourPronouns: String = "She/Her";
+const yourMajor: String = "Cyber Security";
 let classification: String = "junior";
 let oneExcite: String = "Building my first website";
-let anyConcerns: String = "IDK";
+let anyConcerns: String = "N/A";
 let funFact: String = "I am left handed";
